b767440050943b334dbac300f57e3f5fa528cf1a
Lacour Vincent <lacour.vincent.app@gmail.com>
2019-09-17 22:18:40 +0200

    Add custom toolbar on android welcome page

--- android/app/src/main/java/com/lacour/vincent/wificaresp8266/Welcome.kt
+++ android/app/src/main/java/com/lacour/vincent/wificaresp8266/Welcome.kt
@@ -2,11 +2,28 @@ package com.lacour.vincent.wificaresp8266
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class Welcome : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_activity)
+        setSupportActionBar(findViewById(R.id.toolbar_welcome))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_welcome_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            R.id.action_wifi -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 }
