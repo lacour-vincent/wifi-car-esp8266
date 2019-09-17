2d29c336473b47de5bb9925719b408a8d4336127
Lacour Vincent <lacour.vincent.app@gmail.com>
2019-09-17 22:04:28 +0200

    Add resources android files

--- android/app/src/main/java/com/lacour/vincent/wificaresp8266/Welcome.kt
+++ android/app/src/main/java/com/lacour/vincent/wificaresp8266/Welcome.kt
@@ -3,10 +3,10 @@ package com.lacour.vincent.wificaresp8266
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Welcome : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.welcome_activity)
     }
 }
